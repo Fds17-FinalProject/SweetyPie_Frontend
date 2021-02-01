d18432aef9e475061ba1d89bbd8903708280d5a9
hanseul-lee <hanseul.dev@gmail.com>
2021-02-01 11:45:26 +0900

    initiate SweetyPie_Frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import './assets/output.css'
+import './assets/output.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">안녕</div>;
 }
 
 export default App;
