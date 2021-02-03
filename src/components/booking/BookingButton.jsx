7147e9d8cf592ef4891aa4bbee2d639b6351c64c
dongchan-K <ehdcks1028@gmail.com>
2021-02-03 18:34:06 +0900

    feat: BookingButton 컴포넌트 구현 (#5)

---
+++ src/components/booking/BookingButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SVG from '../../assets/Svg';
+
+const BookingButton = () => {
+  return (
+    <div className="mt-10 flex">
+      <button className="flex items-center justify-center w-60 h-20 mr-10 bg-#D70466 text-white font-bold rounded-2xl relative">
+        <SVG
+          name="lock"
+          width="16px"
+          height="16px"
+          viewBox="0 0 24 24"
+          color="#fff"
+        />
+        <span className="m-2 text-2xl">수정 완료</span>
+      </button>
+      <button className="flex items-center justify-center w-60 h-20 bg-gray-400 text-black font-bold rounded-2xl">
+        <span className="m-2 text-2xl">예약 취소</span>
+      </button>
+    </div>
+  );
+};
+
+export default BookingButton;
