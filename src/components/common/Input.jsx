583daa27a9ebe7de3196707870d0b04e0d7df608
dongchan-K <ehdcks1028@gmail.com>
2021-02-03 19:00:17 +0900

    feat: 공통 Input 컴포넌트 구현

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
-const Input = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Input = ({ placeholderText }) => {
+  return (
+    <input
+      placeholder={placeholderText}
+      type="text"
+      className="w-full h-20 my-5 border rounded-lg px-6"
+    />
+  );
 };
 
 export default Input;
