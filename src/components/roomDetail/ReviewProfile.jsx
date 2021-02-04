0bfa2ec0338450e6bb9e4eefb1e1793250ee0706
hanseul-lee <hanseul.dev@gmail.com>
2021-02-05 05:41:47 +0900

    feat: ReviewProfile 컴포넌트 날짜 년,월만 나오도록 메소드 추가 (#2)

--- src/components/roomDetail/ReviewProfile.jsx
+++ src/components/roomDetail/ReviewProfile.jsx
@@ -2,23 +2,9 @@ import React from 'react';
 
 const ReviewProfile = ({ name, photo }) => {
   const img = window.location.origin;
+  const createdDate = '2021 - 02 - 06';
   const content =
     '거실쪽은 생각보다 좁았는데 세명이 쓰기엔 괜찮았고 깔끔해서 좋았어요. 화장실도 깨끗했고 침실도 깨끗하고 폭신폭신해서 좋았습니다. 숙소뷰로 호수가 보여서 좋았구요 해 뜨는것도 보이더라구요ㅠㅠ 다음에 또 이용하고싶습니다 ㅎㅎ';
-  // const createdDate = '2021 - 02 - 02';
-  const createdDate = '2021년 02월';
-  const guestNames = [
-    '한슬',
-    '영서',
-    '동찬',
-    '재민',
-    '영원',
-    '재복',
-    '준철',
-    'Hanseul',
-    'JamJam',
-    'Mark',
-  ];
-  const guestName = guestNames[Math.floor(Math.random() * 10)];
 
   return (
     <div className="mr-9.5rem">
@@ -31,7 +17,9 @@ const ReviewProfile = ({ name, photo }) => {
         <div className="">
           {/* <div className="">{guestName}</div> */}
           <div className="text-1.6rem font-bold">{name}</div>
-          <div className="text-1.4rem text-#717171">{createdDate}</div>
+          <div className="text-1.4rem text-#717171">
+            {createdDate.split(' -')[0]}년 {createdDate.split(' -')[1]}월
+          </div>
         </div>
       </div>
       <div className="mb-20 text-1.6rem">{content}</div>
