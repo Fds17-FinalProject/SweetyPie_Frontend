508b515544a94a7019874dd220c17777f3738c8c
Kim-Jaemin420 <jany240@naver.com>
2021-02-05 11:08:15 +0900

    feat: 검색 결과 페이지 (#1)

---
+++ src/containers/SearchContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchTemplate from '../components/Search/SearchTemplate';
+
+const SearchContainer = () => {
+  return <SearchTemplate />;
+};
+
+export default SearchContainer;
