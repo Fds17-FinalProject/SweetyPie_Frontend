feede6c47f3402bf98f2f6dcf370024659aa20cb
Kim-Jaemin420 <jany240@naver.com>
2021-02-05 11:03:01 +0900

    feat: 숙소 유형 필터 모달 구현 (#1)

---
+++ src/components/Search/Checkbox.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  input[type=checkbox] {
+  display: none;
+  }
+
+  .bgholder {
+    width: 30px;
+    height: 30px;
+    position: relative;
+    background-color: black;
+  }
+
+  input[type=checkbox] + label
+  {
+    border: 1px solid;
+    border-radius: 5px;
+    position: absolute;
+    top: ${({top}) => top};
+    left: 12px;
+    height: 25px;
+    width: 25px;
+    line-height: 60px;
+    display:inline-block;
+  }
+  input[type=checkbox]:checked + label
+  {
+    background: url('https://png.pngtree.com/png-clipart/20190516/original/pngtree-tick-vector-icon-png-image_4236552.jpg') no-repeat;
+    background-size: 100%;
+    background-position: center;
+    display:inline-block;
+  }
+`;
+
+
+
+const Checkbox = ({ name, top }) => {
+  return (
+    <>
+      <Div name={name} top={top}>
+        <input type='checkbox' name={name} id={name} />
+        <label htmlFor={name} top={top}></label>
+      </Div>
+    </>
+  );
+};
+
+export default Checkbox;
