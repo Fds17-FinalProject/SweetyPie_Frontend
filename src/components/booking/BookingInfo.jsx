6c21a776fe9e96e2e6279bb6ba57431e70818f35
dongchan-K <ehdcks1028@gmail.com>
2021-02-05 11:17:06 +0900

    feat: 모달 관련 로직 추가 (#6)

--- src/components/booking/BookingInfo.jsx
+++ src/components/booking/BookingInfo.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const BookingInfo = () => {
+const BookingInfo = ({
+  checkInDate,
+  checkoutDate,
+  guestNum,
+  bookingEdit,
+  showModal
+}) => {
   return (
     <section className="mt-10">
       <div>
@@ -8,17 +14,28 @@ const BookingInfo = () => {
       </div>
       <div className="mt-10 relative">
         <p className="text-1.6rem font-semibold">날짜</p>
-        <button className="text-1.6rem font-semibold underline absolute right-0 top-0">
-          수정
-        </button>
-        <div className="text-1.6rem text-#717171 mt-3">2021.3.16-2021.3.21</div>
+        {bookingEdit && (
+          <button
+            className="text-1.6rem font-semibold underline absolute right-0 top-0"
+            onClick={showModal}
+          >
+            수정
+          </button>
+        )}
+        <div className="text-1.6rem text-#717171 mt-3">
+          {`${checkInDate}-${checkoutDate}`}
+        </div>
       </div>
       <div className="mt-10 relative">
         <p className="text-1.6rem font-semibold">게스트</p>
-        <button className="text-1.6rem font-semibold underline absolute right-0 top-0">
-          수정
-        </button>
-        <div className="text-1.6rem text-#717171 mt-3">게스트 1명</div>
+        {bookingEdit && (
+          <button className="text-1.6rem font-semibold underline absolute right-0 top-0">
+            수정
+          </button>
+        )}
+        <div className="text-1.6rem text-#717171 mt-3">
+          {`게스트 ${guestNum}명`}
+        </div>
       </div>
     </section>
   );
