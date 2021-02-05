df5360836e25ff494639fa1e0d16757979d0d2d1
dongchan-K <ehdcks1028@gmail.com>
2021-02-05 11:20:10 +0900

    feat: 예약 수정 및 결제 페이지 조건에 따른 렌더링 조건 추가 (#6)

--- src/components/booking/BookingTitle.jsx
+++ src/components/booking/BookingTitle.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdNavigateBefore } from 'react-icons/md';
 
-const BookingTitle = () => {
+const BookingTitle = ({ bookingEdit }) => {
   return (
     <div className="flex items-center pb-7 px-32 pt-6.4rem mx-48">
-      <div className="w-20 h-20 -ml-8 flex items-center justify-center rounded-half hover:bg-#f7f7f7">
-        <Link>
+      <div className="w-20 h-20 -ml-8 flex items-center justify-center rounded-50% hover:bg-#f7f7f7">
+        <Link to="/reservation">
           <MdNavigateBefore className="w-10 h-10" />
         </Link>
       </div>
-      <h1 className="ml-10 text-3.2rem font-semibold">예약 수정하기</h1>
+      {bookingEdit ? (
+        <h1 className="ml-10 text-3.2rem font-semibold">예약 수정하기</h1>
+      ) : (
+        <h1 className="ml-10 text-3.2rem font-semibold">예약 요청하기</h1>
+      )}
     </div>
   );
 };
