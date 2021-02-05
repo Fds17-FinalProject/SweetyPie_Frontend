82f97fb34bb31978f7550756987fb4093a33340f
youngseo-im <gg2922914@gmail.com>
2021-02-05 11:00:13 +0900

    style: 불필요한 코드 삭제

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,49 +1,28 @@
-// import React, { useState, useEffect, useCallback, useRef } from 'react';
-// import ReactDOM from 'react-dom';
-
-// import Header from '../components/Main/Header';
-// import CommonHeader from '../components/common/CommonHeader';
-// import Main from '../components/Main/Main';
-// import Footer from '../components/Main/Footer';
-
-// const MainContainer = () => {
-//     const [scrollY, setScrollY] = useState(0);
-//           useEffect(() => {
-//               function logit() {
-//                 setScrollY(window.pageYOffset);
-//               }
-//              (function watchScroll() {
-//                 window.addEventListener('scroll', logit);
-//           }());
-//           return () => {
-//             window.removeEventListener('scroll', logit);
-//           };
-//         },[scrollY]);
-
-//   return (
-//       <div className="flex-column flex-nowrap relative">
-//       {scrollY > 30 ? <CommonHeader/> : <Header />}
-//       <Main />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default MainContainer;
-
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import ReactDOM from 'react-dom';
-
 import Header from '../components/Main/Header';
 import CommonHeader from '../components/common/CommonHeader';
 import Main from '../components/Main/Main';
 import Footer from '../components/Main/Footer';
-
+import Modal from '../components/common/Modal';
+import UserMenu from '../components/Main/userMenu';
+  
 const MainContainer = () => {
+      const [visible, setVisible] = useState(false);
+      const modalElement = useRef(null);
+      const showModal = () => {
+        setVisible(true);
+      };
+  
+      const hideModal = modalElement => {
+            if (modalElement.target.id !== '1') return;
+            setVisible(false);
+          };
+
     const [scrollY, setScrollY] = useState(0);
           useEffect(() => {
               function logit() {
-                  setScrollY(window.pageYOffset);
+                setScrollY(window.pageYOffset);
+                // scrollY === 30 ? 
               }
              (function watchScroll() {
                 window.addEventListener('scroll', logit);
@@ -53,11 +32,16 @@ const MainContainer = () => {
           };
         },[scrollY]);
 
+
+
   return (
-      <div className="flex-column flex-nowrap relative">
-          {scrollY > 30 ? <CommonHeader/> : <Header />}
-          {/* <Header /> */}
-      {/* <CommonHeader /> */}
+    <div className="flex-column flex-nowrap relative">
+      {visible &&
+        <Modal>
+        <UserMenu hideModal={hideModal}/>
+      </Modal>}
+          <Header showModal={showModal} modalElement={modalElement} visible={visible}/>
+      <CommonHeader showModal={showModal} modalElement={modalElement} visible={visible}/>
       <Main />
       <Footer />
     </div>
