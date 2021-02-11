e4a3ccf34d7f4b6b84d3c8c4dfd88fced6fab9a5
dongchan-K <ehdcks1028@gmail.com>
2021-02-12 03:10:25 +0900

    etc: 경로, 파일 이름 변경 및 컨벤션 통일

--- src/components/Search/Pagination.jsx
+++ src/components/Search/Pagination.jsx
@@ -2,47 +2,38 @@ import React, { useState, useEffect } from 'react';
 // import axios from 'axios'
 import ReactPaginate from 'react-paginate';
 import '../../index.css';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import axios from 'axios';
-import { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useLocation } from 'react-router-dom';
-import { getList } from '../../redux/lib/api';
-import { getAccommList } from '../../redux/modules/accommodations';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Pagination() {
   const [offset, setOffset] = useState(0);
-  const [data, setData] = useState([]);
-  const [perPage] = useState(10);
-  const [pageCount, setPageCount] = useState(0);
-  const location = useLocation();
 
   const accommodations = useSelector(state => state.accommodations);
-  
+
   const history = useHistory();
   const handlePageClick = e => {
     const url = new URL(window.location.href);
     const selectedPage = e.selected;
     setOffset(selectedPage + 1);
-    if (!url.searchParams.get('page')) { 
+    if (!url.searchParams.get('page')) {
       url.searchParams.append('page', selectedPage + 1);
       console.log('페이지 없을 경우 추가해서 값 넣어준다');
       history.push(url.search);
       window.scrollTo(0, 0);
     }
-    
-    if (url.searchParams.get('page')) { 
+
+    if (url.searchParams.get('page')) {
       console.log('페이지 있는 경우에는 덮어쓰기');
 
       url.searchParams.set('page', selectedPage + 1);
       history.push(url.search);
       window.scrollTo(0, 0);
-    } 
+    }
   };
 
   useEffect(() => {
     // getData();
-  }, [ offset]);
+  }, [offset]);
 
   return (
     <div>
--- src/pages/ModifyAccountPage.js
+++ src/pages/ModifyAccountPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ModifyAccountContainer from '../containers/ModifyAccountContainer';
 
-const ModifyAccountPage = (props) => {
+const ModifyAccountPage = props => {
   console.log(props);
   return (
     <div>
