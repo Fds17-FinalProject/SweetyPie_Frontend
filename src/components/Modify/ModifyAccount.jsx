b33701886a032e20c153c38994dbadb12b804df3
hanseul-lee <hanseul.dev@gmail.com>
2021-02-11 17:08:07 +0900

    etc: merge과정에서 conflict 해결

--- src/components/Modify/ModifyAccount.jsx
+++ src/components/Modify/ModifyAccount.jsx
@@ -17,53 +17,26 @@ const res = {
 const ModifyAccount = ({ onClick, modify }) => {
   return (
     <div>
-      <header
-<<<<<<< HEAD
-        className="
-        max-w-screen-2xl px-32 bg-white w-full h-32 flex items-center justify-between	fixed z-10
-         "
-      >
-=======
-        className="max-w-screen-2xl px-32 bg-white w-full h-32 flex items-center justify-between	fixed z-10">
->>>>>>> 603f37e3412d3ba5e3298db53b00d1014c9f8955
+      <header className="max-w-screen-2xl px-32 bg-white w-full h-32 flex items-center justify-between	fixed z-10">
         <HeaderLogo color="#FF385C" />
         <HeaderUser />
       </header>
       <div className="max-w-screen-2xl w-full flex-grow-1 px-72 pt-32 min-h-75rem">
         <h1 className="a11y-hidden">개인정보 수정</h1>
-<<<<<<< HEAD
-        {/* <Link className="text-#727272 text-1.4rem">계정</Link> */}
-=======
->>>>>>> 603f37e3412d3ba5e3298db53b00d1014c9f8955
         <h2 className="text-3.2rem font-extrabold text-#727272 py-16">
           개인정보
         </h2>
         <div className="max-w-screen-2xl flex justify-between ">
           {/* left */}
           <div className=" w-3/5 pt-8">
-<<<<<<< HEAD
-            {modify.email ? (
-              <ModifyInput name="email" onClick={onClick} content={res.email}>
-                이메일 수정
-              </ModifyInput>
-            ) : (
-              <ModifyContent
-                htmlFor="email-input"
-                id="email-input"
-                name="email"
-                onClick={onClick}
-                content={res.email}
-              >
-                이메일
-              </ModifyContent>
-            )}
-
-=======
             <div className=" flex relative flex-wrap border-b border-#rgb235 pt-0.4rem pb-2.4rem">
-              <span className="w-full text-1.6rem font-semibold text-#727272">이메일</span>
-              <span className="w-full pt-2 text-1.6rem font-normal text-#717171">{res.email}</span>
-           </div>
->>>>>>> 603f37e3412d3ba5e3298db53b00d1014c9f8955
+              <span className="w-full text-1.6rem font-semibold text-#727272">
+                이메일
+              </span>
+              <span className="w-full pt-2 text-1.6rem font-normal text-#717171">
+                {res.email}
+              </span>
+            </div>
             {modify.name ? (
               <ModifyInput name="name" onClick={onClick} content={res.name}>
                 이름 수정
