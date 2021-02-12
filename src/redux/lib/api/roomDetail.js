184abd7962f25c6086a96d8778a81449b27f21a7
hanseul-lee <hanseul.dev@gmail.com>
2021-02-12 15:38:05 +0900

    feat: roomDetail module 구현

--- src/redux/lib/api/roomDetail.js
+++ src/redux/lib/api/roomDetail.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 // 예약 내역 GET 요청
-export const roomDetail = id =>
+export const readRoomDetail = id =>
   axios.get(`http://3.34.50.91/api/accommodation/${id}`);
---
+++ src/redux/modules/roomDetail.js
@@ -0,0 +1,48 @@
+import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as authAPI from '../lib/api/roomDetail';
+import { takeLatest } from 'redux-saga/effects';
+
+// action types
+const [
+  READ_ROOMDETAIL,
+  READ_ROOMDETAIL_SUCCESS,
+  READ_ROOMDETAIL_FAILURE,
+] = createRequestActionTypes('roomDetail/READ_ROOMDETAIL');
+
+// action creators
+export const readRoomDetail = createAction(
+  READ_ROOMDETAIL,
+  ({ accommodation }) => ({
+    accommodation,
+  }),
+);
+
+// saga
+const readRoomDetailSaga = createRequestSaga(
+  READ_ROOMDETAIL,
+  authAPI.readRoomDetail,
+);
+export function* RoomDetailSaga() {
+  yield takeLatest(READ_ROOMDETAIL, readRoomDetailSaga);
+}
+
+// initialState
+const initialState = {
+  accommodation: {},
+  roomDeatilError: null,
+};
+
+// reducer
+const roomDetail = handleActions({
+  [READ_ROOMDETAIL_SUCCESS]: (state, { payload: { accommodation } }) => ({
+    ...state,
+    accommodation,
+  }),
+  [READ_ROOMDETAIL_FAILURE]: (state, { payload: { error } }) => ({
+    ...state,
+    roomDeatilError: error,
+  }),
+});
