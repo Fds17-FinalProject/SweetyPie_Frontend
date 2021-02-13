f9ad1b38599d40ed945a77ba4be267e3e4e73839
hanseul-lee <hanseul.dev@gmail.com>
2021-02-13 16:46:23 +0900

    feat: RoomDetail 페이지  Photos 컴포넌트 api 요청(GET)

--- src/components/roomDetail/Photos.jsx
+++ src/components/roomDetail/Photos.jsx
@@ -1,54 +1,41 @@
 import React from 'react';
 
-const Photos = () => {
-  const img = window.location.origin;
-  const accommodationPictures = [
-    {
-      url:
-        'https://a0.muscache.com/pictures/1c1654ed-0f90-458f-a577-aa4a2e2c1462.jpg',
-    },
-    { url: 'window.location.origin/img/room01.jpg' },
-    { url: 'window.location.origin/img/room02.jpg' },
-    { url: 'window.location.origin/img/room03.jpg' },
-    { url: 'window.location.origin/img/room04.jpg' },
-    { url: 'window.location.origin/img/room05.jpg' },
-  ];
-  console.log(accommodationPictures);
+const Photos = ({ accommodationPictures }) => {
   return (
     <div className="mt-2.4rem h-38.8rem w-full rounded-3xl flex justify-between overflow-hidden">
       <div className="w-56.4rem ">
         <img
           className="block w-full h-full object-cover	"
-          src={img + '/img/room01.jpg'}
-          alt="..."
-        />
-      </div>
-      <div className="w-27.4rem ml-0.8rem">
-        <img
-          className="block w-full h-19.4rem mb-0.8rem object-cover"
           src={accommodationPictures[0].url}
-          alt="..."
-        />
-        <img
-          className="block w-full h-19.4rem object-cover"
-          src={img + '/img/room04.jpg'}
-          alt="..."
+          alt="accommodationPicture_01"
         />
       </div>
       <div className="w-27.4rem ml-0.8rem">
         <img
           className="block w-full h-19.4rem mb-0.8rem object-cover"
-          src={img + '/img/room03.jpg'}
-          alt="..."
+          src={accommodationPictures[1].url}
+          alt="accommodationPicture_02"
         />
         <img
           className="block w-full h-19.4rem object-cover"
-          src={img + '/img/room05.jpg'}
-          alt="..."
+          src={accommodationPictures[2].url}
+          alt="accommodationPicture_03"
+        />
+      </div>
+      <div className="w-27.4rem ml-0.8rem">
+        <img
+          className="block w-full h-19.4rem mb-0.8rem object-cover"
+          src={accommodationPictures[3].url}
+          alt="accommodationPicture_04"
+        />
+        <img
+          className="block w-full h-19.4rem object-cover"
+          src={accommodationPictures[4].url}
+          alt="accommodationPicture_05"
         />
       </div>
     </div>
   );
 };
 
-export default Photos;
+export default React.memo(Photos);
