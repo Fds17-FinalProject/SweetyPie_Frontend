ff9f3d9eee8c418820ef51126155ef3f25d5087c
hanseul-lee <hanseul.dev@gmail.com>
2021-02-21 02:03:30 +0900

    style: 변수명 room을 accommodation로 통일

--- src/containers/RoomDetailContainer.js
+++ src/containers/RoomDetailContainer.js
@@ -3,29 +3,45 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import RoomDetailTemplate from '../components/roomDetail/RoomDetailTemplate';
 import { readRoomDetail } from '../redux/modules/roomDetail';
+import { postBookmark, deleteBookmark } from '../redux/lib/api/bookmark';
 
 const RoomDetailContainer = () => {
   // const accommodation2 = useSelector(state => console.log(state));
-  const { accommodation, roomDetailError, loading } = useSelector(
+  const { accommodation, accommodationError, loading } = useSelector(
     ({ roomDetail, loading }) => ({
       accommodation: roomDetail.accommodation,
-      roomDetailError: roomDetail.roomDetailError,
+      accommodationError: roomDetail.accommodationError,
       loading: loading['roomDetail/READ_ROOMDETAIL'],
     }),
   );
   // console.log(accommodation);
 
   const dispatch = useDispatch();
-  // url의 room_id 가져오기
-  const { room_id } = useParams();
+  // url의 accommodation_id 가져오기
+  const { accommodation_id } = useParams();
 
   useEffect(() => {
-    dispatch(readRoomDetail(room_id));
-  }, [dispatch, room_id]);
+    dispatch(readRoomDetail(accommodation_id));
+  }, [dispatch, accommodation_id]);
+
+  // 북마크 등록 (POST)
+  const postBookmarkRoom = accommodation_id => {
+    postBookmark(accommodation_id);
+  };
+
+  // 북마크 제거 (DELETE)
+  const deleteBookmarkedRoom = accommodation_id => {
+    deleteBookmark(accommodation_id);
+  };
 
   return (
     <div>
-      <RoomDetailTemplate accommodation={accommodation} loading={loading} />
+      <RoomDetailTemplate
+        accommodation={accommodation}
+        loading={loading}
+        postBookmark={postBookmarkRoom}
+        deleteBookmark={deleteBookmarkedRoom}
+      />
     </div>
   );
 };
