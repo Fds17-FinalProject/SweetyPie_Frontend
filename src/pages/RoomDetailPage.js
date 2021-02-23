f4b84364c1ba93e5db90f467b5ab4e4d78c47924
hanseul-lee <hanseul.dev@gmail.com>
2021-02-24 00:43:38 +0900

    feat: RoomDeatilPage에 Helmet 적용

--- src/pages/RoomDetailPage.js
+++ src/pages/RoomDetailPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import RoomDetailContainer from '../containers/RoomDetailContainer';
-// import { Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 
 const RoomDetailPage = () => {
   return (
     <>
-      {/* <Helmet> */}
-      {/* <title>숙소 상세보기 - Airbnb</title> */}
-      <title>💛스위티파이 쪼앙💛</title>
-      {/* </Helmet> */}
+      <Helmet>
+        {/* <title>숙소 상세보기 - Airbnb</title> */}
+        <title>💛스위티파이 쪼앙💛</title>
+      </Helmet>
       <div>
         <RoomDetailContainer />
       </div>
