7e2b63e963957c927e1edd245dc9b2fda66d0903
hanseul-lee <hanseul.dev@gmail.com>
2021-02-24 23:42:24 +0900

    feat: Header 3단변신 구현

--- src/components/roomDetail/RoomDetailTemplate.jsx
+++ src/components/roomDetail/RoomDetailTemplate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../assets/output.css';
 import Photos from './Photos';
 import Introduction from './Introduction';
